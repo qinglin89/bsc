bd300998c1fc9c9be5cda806b9cc85c55835dd6d
qinglin89 <316032931@qq.com>
2023-02-07 09:29:12 +0800

    refactor: comment test print

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -21,7 +21,6 @@ import (
 	"fmt"
 	"io"
 	"math/big"
-	"reflect"
 	"sync"
 	"time"
 
@@ -278,37 +277,37 @@ func (s *StateObject) GetCommittedState(db Database, key common.Hash, hit *bool,
 				cachemetrics.RecordMinerTotalCosts("MINER_L1_STORAGE", start)
 			}
 		}
-		var testMap sync.Map
-		testKey := common.HexToHash("0x75808d711721ca136a472157da58f24790bdf36249c43af0a279803a3f4794e334")
-		testValue := common.HexToHash("0x75808d11721ca136a472157da58f24790bdf36249c43af0a279321321321233")
-		testMap.Store(testKey, testValue)
-		v, cacahed := testMap.Load(testKey)
-		if cacahed {
-			fmt.Println("test map get value succ , key ,value", testKey, testValue)
-			fmt.Println("test map key tepe", reflect.TypeOf(v))
-		} else {
-			fmt.Println("test map get value fail")
-		}
-
-		testMap.Range(func(key, value interface{}) bool {
-			k := key.(common.Hash)
-			v := value.(int)
-			fmt.Println("test map range value:", k, v)
-			//fmt.Println("test map range value:", k, v)
-			fmt.Println("test map range type", reflect.TypeOf(k))
-			fmt.Println("test map range type", reflect.TypeOf(v))
-			return true
-		})
-
-		s.setOriginStorage(testKey, testValue)
-		v2, cacahed2 := s.getOriginStorage(testKey)
-		if cacahed2 {
-			fmt.Println("test map2 get value succ , key ,value", testKey, testValue)
-			fmt.Println("test map2 key tepe", reflect.TypeOf(v2))
-		} else {
-			fmt.Println("test map2 get value fail")
-		}
-
+		//		var testMap sync.Map
+		//		testKey := common.HexToHash("0x75808d711721ca136a472157da58f24790bdf36249c43af0a279803a3f4794e334")
+		//		testValue := common.HexToHash("0x75808d11721ca136a472157da58f24790bdf36249c43af0a279321321321233")
+		//		testMap.Store(testKey, testValue)
+		//		v, cacahed := testMap.Load(testKey)
+		//		if cacahed {
+		//			fmt.Println("test map get value succ , key ,value", testKey, testValue)
+		//			fmt.Println("test map key tepe", reflect.TypeOf(v))
+		//		} else {
+		//			fmt.Println("test map get value fail")
+		//		}
+		//
+		//		testMap.Range(func(key, value interface{}) bool {
+		//			k := key.(common.Hash)
+		//			v := value.(int)
+		//			fmt.Println("test map range value:", k, v)
+		//			//fmt.Println("test map range value:", k, v)
+		//			fmt.Println("test map range type", reflect.TypeOf(k))
+		//			fmt.Println("test map range type", reflect.TypeOf(v))
+		//			return true
+		//		})
+		//
+		//		s.setOriginStorage(testKey, testValue)
+		//		v2, cacahed2 := s.getOriginStorage(testKey)
+		//		if cacahed2 {
+		//			fmt.Println("test map2 get value succ , key ,value", testKey, testValue)
+		//			fmt.Println("test map2 key tepe", reflect.TypeOf(v2))
+		//		} else {
+		//			fmt.Println("test map2 get value fail")
+		//		}
+		//
 	}()
 	// If the fake storage is set, only lookup the state here(in the debugging mode)
 	if s.fakeStorage != nil {
